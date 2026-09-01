1d8aefe5efe6402208e7802ea337e2e9d766dbac
Sen <senjutipaul07@gmail.com>
2026-09-02 01:44:56 +0530

    added questions

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,9 @@ import './App.css'
 import Navbar from './components/navbar'
 import InpInputBox from './components/inp-input-box'
 import InpRadioButton from './components/inp-radio-button'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Form() {
   return (
     <>
       <Navbar />
@@ -18,7 +17,7 @@ function App() {
       <br></br>
       <InpInputBox question="Email" />
       <br></br>
-      <h3 className="q" style={{padding: "0 0 0 100px" , color:"white", fontWeight:"500"}}>Branch</h3>
+      <h3 className="q" style={{ padding: "0 0 0 100px", color: "white", fontWeight: "500" }}>Branch</h3>
       <br></br>
       <InpRadioButton option="CSE" />
       <InpRadioButton option="CSE-AIML" />
@@ -28,7 +27,15 @@ function App() {
       <br></br>
       <InpInputBox question="Roll Number" />
       <br></br>
-      <h3 className="q" style={{padding: "0 0 0 100px" , color:"white", fontWeight:"500"}}>Domain you want to apply for</h3>
+      <InpInputBox question="Tell us about yourself" />
+      <br></br>
+      <InpInputBox question="Why do you want to join ISTE?" />
+      <br></br>
+      <InpInputBox question="How will you help in the growth of the society?" />
+      <br></br>
+      <InpInputBox question="What do you expect to learn from ISTE?" />
+      <br></br>
+      <h3 className="q" style={{ padding: "0 0 0 100px", color: "white", fontWeight: "500" }}>Domain you want to apply for</h3>
       <br></br>
       <InpRadioButton option="Technical" />
       <InpRadioButton option="Content Writing" />
@@ -36,11 +43,25 @@ function App() {
       <InpRadioButton option="Photography" />
       <InpRadioButton option="Video Editing" />
       <InpRadioButton option="Public Relations and Management" />
-      <InpRadioButton option="Video Editing" />
       <br></br>
 
     </>
   )
+}
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="/" element={<Form />} />
+      {/* <Route path="/technical" element={<Technical />} />
+      <Route path="/content-writing" element={<ContentWriting />} />
+      <Route path="/graphic-designing" element={<GraphicDesigning />} />
+      <Route path="/photography" element={<Photography />} />
+      <Route path="/video-editing" element={<VideoEditing />} />
+      <Route path="/public-relations-and-management" element={<PublicRelationsAndManagement />} /> */}
+    </Routes>
+  )
+
 }
 
 export default App
