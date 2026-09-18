b8c0cdbecf397260a76db6e6685241b9b22cab98
Sen <senjutipaul07@gmail.com>
2026-09-18 12:54:43 +0530

    backend connection

--- src/components/final.jsx
+++ src/components/final.jsx
@@ -9,17 +9,27 @@ const Final = () => {
             <div id="final">
                 Thank You for filling the form. We will get back to you soon. Stay tuned for further updates.
             </div>
-            <button className="submit-button" onClick={() => {
-                alert("Are you sure you want to submit the form? Once submitted, you won't be able to make any changes.")
+            <button className="submit-button" onClick={async () => {
                 if (window.confirm("Are you sure you want to submit the form? Once submitted, you won't be able to make any changes.")) {
-                    localStorage.removeItem("formData")
-                    window.location.href = "/submitted"
+
+                    const formData = JSON.parse(localStorage.getItem("formData"));
+
+                    const data = new URLSearchParams();
+                    data.append("data", JSON.stringify(formData));
+
+                    await fetch("https://script.google.com/macros/s/AKfycby9Pjhl4kpJ4FlV1p3xRyrMDkDEDB9quIWYSAQZ_7uJmLLeoGxyKfbwmcrLJ7UcqGfj/exec", {
+                        method: "POST",
+                        body: data
+                    });
+
+                    localStorage.removeItem("formData");
+                    window.location.href = "/submitted";
                 }
             }}>
                 Submit
             </button>
         </>
-    
+
     )
 }
 
